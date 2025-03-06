b6307d0740999193717e738187eb7bae64a6ec77
Rahiq Al-Makhtum <rahiq798@gmail.com>
2025-03-06 21:59:56 +0600

    add script.js

--- src/script.js
+++ src/script.js
@@ -4,13 +4,3 @@ function handleMenu(){
     navDialog.classList.toggle('hidden');
 }
 
-let persons = {
-    names: ['setu' , 'rahiq', 'rana'],
-    sayHello: function (){
-        console.log('Hello' + this.names);
-        
-    }
-}
-
-
-persons.sayHello();
